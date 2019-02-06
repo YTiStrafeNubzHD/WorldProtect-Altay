6d6da424e742f03aa3e59877f20ee3e58156a230
iStrafeNubzHDyt <pixelgameskontakt@gmail.com>
2019-02-06 19:58:04 +0100

    test

--- src/aliuly/worldprotect/Main.php
+++ src/aliuly/worldprotect/Main.php
@@ -201,10 +201,10 @@ class Main extends BasicPlugin implements CommandExecutor,Listener {
 	//
 	//////////////////////////////////////////////////////////////////////
 	public function onLevelLoad(LevelLoadEvent $e) {
-		$this->loadCfg($e->$this->getServer()->getLevelManager()->getLevel());
+		$this->loadCfg($e->getServer()->getLevelManager()->getLevel());
 	}
 	public function onLevelUnload(LevelUnloadEvent $e) {
-		$this->unloadCfg($e->$this->getServer()->getLevelManager()->getLevel());
+		$this->unloadCfg($e->getServer()->getLevelManager()->getLevel());
 	}
 
 	//////////////////////////////////////////////////////////////////////
